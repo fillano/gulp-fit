4915d284ae606883e20758591173b7541dd0f817
Hsu Ping Feng <fillano.feng@gmail.com>
2018-10-09 10:41:11 +0800

    project initial.

---
+++ index.js
@@ -0,0 +1,24 @@
+const fit = require('fit-template');
+const through = require('through2');
+const gutil = require('gulp-util');
+const PluginError = gutil.PluginError;
+const PLUGIN_NAME = 'gulp-fit';
+
+module.exports = function(data) {
+    return through.obj(function(file, enc, cb) {
+        if(file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported.'));
+        }
+
+        if(file.isBuffer()) {
+            try {
+                file.contents = Buffer.from(fit(file.contents.toString())(data));
+            } catch(e) {
+                this.emit('error', new PluginError(PLUGIN_NAME, e.toString()));
+            }
+        }
+
+        this.push(file);
+        return cb();
+    });
+};
